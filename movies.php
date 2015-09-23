f3946c0e1fa2a48e5807e29208e56d4d797017a4
Sam Morrison <sammorrison2@me.com>
2015-09-24 09:19:52 +1000

    started doing JSON for movies page

--- movies.php
+++ movies.php
@@ -15,27 +15,9 @@
 				<li id="ironman">
 					<div class="movie-item">
 
-						<img class="movie-img" src="resources/ironman.jpg" alt="">
+						<img class="movie-img" src="" alt="">
 						<p class="movie-description">
-							Tony Stark. Genius, billionaire, playboy, philanthropist. Son of legendary inventor and weapons 
-							contractor Howard Stark. When Tony Stark is assigned to give a weapons presentation to an Iraqi 
-							unit led by Lt. Col. James Rhodes, he's given a ride on enemy lines. That ride ends badly when 
-							Stark's Humvee that he's riding in is attacked by enemy combatants. He survives - barely - with
-							a chest full of shrapnel and a car battery attached to his heart. In order to survive he comes 
-							up with a way to miniaturize the battery and figures out that the battery can power something else.
-							Thus Iron Man is born. He uses the primitive device to escape from the cave in Iraq. Once back home,
-							he then begins work on perfecting the Iron Man suit. But the man who was put in charge of Stark
-							Industries has plans of his own to take over Tony's technology for other matters.- Written by halo1k
-							</br>
-							</br>
-							<strong>Rating</strong>: PG-13 , 126 mins
-							</br>
-							</br>
-							<strong>Category</strong>: Action, Adventure, Sci-fi
-							</br>
-							</br>
-							<button class="displaySessionTimes">Movie Session Times</button>
-							</br>
+							
 						</p>
 
 					</div>
@@ -62,31 +44,15 @@
 								<p><a href="/bookings.php?movie=iron_man&day=sunday&time=9_PM">9:00pm</a></p>
 							</div>
 						</div>
+						<video class="trailer" src=""></video>
 						
 					</div>
 				</li>
 				<li id="tinkerbell">
 					<div class="movie-item">
-						<img class="movie-img" src="resources/tinkerbell.jpg" alt="">
+						<img class="movie-img" src="" alt="">
 						<p class="movie-description">
-							Disneytoon Studios returns to Pixie Hollow with the heartwarming adventure Tinker Bell
-							and the Legend of the NeverBeast. The new story explores an ancient myth of a fabled 
-							creature whose distant roar sparks the curiosity of Tinker Bell's good friend Fawn, 
-							an animal fairy who's not afraid to break the rules to help an animal in need. But this 
-							animal-massive and strange with glowing green eyes-is not really welcome in Pixie Hollow, 
-							and the scout fairies are determined to capture the mysterious beast before it destroys 
-							their home. Fawn, who sees a tender heart beneath his gruff exterior, must convince Tink 
-							and the girls to risk everything to rescue the NeverBeast before time runs out.
-							</br>
-							</br>
-							<strong>Rating</strong>: PG-13 , 121 mins
-							</br>
-							</br>
-							<strong>Category</strong>: Action, Adventure, Sci-fi
-							</br>
-							</br>
-							<button class="displaySessionTimes">Movie Session Times</button>
-							</br>
+							
 						</p>
 
 					</div>
@@ -121,6 +87,7 @@
 								<p><a href="/bookings.php?movie=tinkerbell&day=sunday&time=12_PM">12:00pm</a></p>
 							</div>
 						</div>
+						<video class="trailer" src=""></video>
 						
 					</div>
 				</li>
@@ -128,26 +95,9 @@
 
 				<li id="proposal">
 					<div class="movie-item">
-						<img class="movie-img" src="resources/rom-com.jpg" alt="">
+						<img class="movie-img" src="" alt="">
 						<p class="movie-description">
-						When high-powered book editor Margaret faces deportation to her native Canada,
-							the quick-thinking exec declares that she's actually engaged to her unsuspecting 
-							put-upon assistant Andrew, who she's tormented for years. He agrees to participate 
-							in the charade, but with a few conditions of his own. The unlikely couple heads to 
-							Alaska to meet his quirky family, and the always-in-control city girl finds herself 
-							in one comedic fish-out-of-water situation after another. With an impromptu wedding in
-							the works and an immigration official on their tails, Margaret and Andrew reluctantly 
-							vow to stick to the plan despite the precarious consequences.
-							</br>
-							</br>
-							<strong>Rating</strong>: PG-13 , 117 mins
-							</br>
-							</br>
-							<strong>Category</strong>: Action, Adventure, Sci-fi
-							</br>
-							</br>
-							<button class="displaySessionTimes">Movie Session Times</button>
-							</br>
+							
 						</p>
 
 					</div>
@@ -182,6 +132,7 @@
 								<p><a href="/bookings.php?movie=the_proposal&day=sunday&time=6_PM">6:00pm</a></p>
 							</div>
 						</div>
+						<video class="trailer" src=""></video>
 						
 					</div>
 				</li>
@@ -190,24 +141,9 @@
 				<li id="dancer">
 					<div class="movie-item">
 
-					<img class="movie-img" src="resources/art.jpg" alt="">
+					<img class="movie-img" src="" alt="">
 						<p class="movie-description">
-							Mao's last Dancer tells the true story of Chinese ballet dancer Li Cunxin who grew up in rural
-							poverty in Mao's communist before being given the opportunity to dance in the West in the early
-							80s. Li is forced to examine his conscience as he must choose between his career, family, culture,
-							politics and love whilst having to make heart-wrenching decisions of what he must choose to sacrifice
-							and what he must choose to save.
-							</br>
-							</br>
-							<strong>Rating</strong>: PG-13 , 143 mins
-							</br>
-							</br>
-							<strong>Category</strong>: Action, Adventure, Sci-fi
-							</br>
-							</br>
 							
-							<button class="displaySessionTimes" >Movie Session Times</button>
-							</br>
 						</p>
 
 					</div>
@@ -230,6 +166,7 @@
 								<p><a href="/bookings.php?movie=mao's_last_dancer&day=sunday&time=3_PM">3:00pm</a></p>
 							</div>
 						</div>
+						<video class="trailer" controls src=""></video>
 						
 					</div>
 				</li>
