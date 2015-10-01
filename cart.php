1f0a8abdae3eda82e5bc486ae723c625602745c0
rmit-s3490036-Samuel-Morrison <s3530184@student.rmit.edu.au>
2015-10-01 23:57:03 +0000

    In process of adding cart page

--- cart.php
+++ cart.php
@@ -1,8 +1,9 @@
 <!doctype html>
 <html lang="en">
-<?php include(head.php);
+<?php include(head.php);?>
 <body>
+    <?php include(header.php);?>
     
-    <?php include(footer.php);
+    <?php include(footer.php);?>
 </body>
 </html>
