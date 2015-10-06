6a17dace3115cde9a6ee9093b8325f8d3f0d3cd9
rmit-s3490036-Samuel-Morrison <=>
2015-10-06 07:28:15 +0000

    naomi test

--- customer-details.php
+++ customer-details.php
@@ -11,16 +11,16 @@
 		<div id="contactus-container">
 			<form id="form-content" action="ticket.php" method="post">
 				
-					<label for="name">Name:</label>  <!-- Sorry but this regex doesnt work properly either -->
+					<label for="name">Name:</label>
 					<input id="name" class="customer-details" type="text" name="name" pattern= "[A-Za-z]{15}" autofocus="true" required>
 					<br>
-					<label for="lastname">Lastname:</label> 
+					<label for="lastname">Lastname:</label>
 					<input id="lastname" class="customer-details" type="text" name="lastname">
 					<br>
 					<label for="email">Email</label> 
 					<input id="email" type="email" name="email" autofocus="true" required placeholder="example@email.com">
 					<br>
-					<label for ="mobno">Mobile number:</label>   <!-- Phone number pattern needs to work with +61 as well, lat digit will not be read at the moment -->
+					<label for ="mobno">Mobile number:</label>
 					<input id="mobno" class="customer-details" type="text" name="mobno" patter="((04)|04|+61)([ ]?\d{4})([ ]?\d{4})" title="Australian mobile number required">
 					<br>
 					<br>
