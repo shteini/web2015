fc68493ad3cc69f7ac73528bbf639c53f813b25d
rmit-s3490036-Samuel-Morrison <=>
2015-10-07 05:52:13 +0000

    Naomi- regex update

--- customer-details.php
+++ customer-details.php
@@ -15,13 +15,13 @@
 					<input id="name" class="customer-details" type="text" name="name" pattern= "[A-Za-z]{15}" autofocus="true" required>
 					<br>
 					<label for="lastname">Lastname:</label>
-					<input id="lastname" class="customer-details" type="text" name="lastname">
+					<input id="lastname" class="customer-details" type="text" name="lastname" pattern= "[A-Za-z]{15}">
 					<br>
 					<label for="email">Email:</label> 
 					<input id="email" type="email" name="email" autofocus="true" required placeholder="example@email.com" required>
 					<br>
 					<label for ="mobno">Mobile number:</label>
-					<input id="mobno" class="customer-details" type="text" name="mobno" pattern="((04)|04|+61)([ ]?\d{4})([ ]?\d{4})" title="Australian mobile number required" required>
+					<input id="mobno" class="customer-details" type="text" name="mobno" pattern="((04)|04|+614)([ ]?\d{4})([ ]?\d{4})" title="Australian mobile number required" required>
 					<br>
 					<br>
 					<input id="submit" type="submit" value="Submit">
